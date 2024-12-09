75b77315faf816de283dfb9526eb6da27e3e2c7e
Amna Shaheena <119941495+AmnaShaheena@users.noreply.github.com>
2024-12-09 11:03:26 +0000

    modified

--- UC/adduser.php
+++ UC/adduser.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../conf/dbconf.php'
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
