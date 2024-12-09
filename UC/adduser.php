8eb8949186da05b2472a5e415ef9971ea7babd7e
Amna Shaheena <119941495+AmnaShaheena@users.noreply.github.com>
2024-12-09 11:02:34 +0000

    added adduser

---
+++ UC/adduser.php
@@ -0,0 +1,13 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+?>
